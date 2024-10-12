0fb7956fb02c039efb3ef89ce55d37c2793deb59
Benjamin Torfu <btorfu@proton.me>
2024-10-12 21:01:24 +0000

    server modification

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,35 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/index');
 const dbClient = require('./utils/db');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const PORT = process.env.PORT || 5000
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.use(morgan('dev'));
+app.use(helmet());
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
 });
+
+
+dbClient.client.connect()
+    .then(() =>{
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error('Database connection failed:', error);
+    });
+
+    process.on('SIGINT', async () => {
+        await dbClient.client.close();
+        console.log('MongoDB disconnected');
+        process.exit(0);
+    })
