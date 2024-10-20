37f7d6524bc59ab9a2dfc06c545660a8dabf546c
Benjamin Torfu <btorfu@proton.me>
2024-10-20 08:09:42 +0000

    rendring header, home and footer in the index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
-import Routter from './router'; // Use a common App component for routes
-// import HomePage from './Homepage';
+// import Routter from './router'; // Use a common App component for routes
+import Home from './home';
+import Header from './header';
+import Footer from './footer';
+// import Layout from './layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Routter />
-     {/* <HomePage /> */}
+      {/* <Layout /> */}
+     <Header />
+     <Home />
+     <Footer />
+     {/* <Routter /> */}
      </Router>
 
 
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './layout'; 
-import Home from './home';
+// import Layout from './layout'; 
+// import Home from './home';
 import RoleSelection from './role';
 import Signup from './signup';
 import Login from './login';
@@ -13,16 +13,16 @@ const Routter = () => {
   return (
     <Routes>
       {/* Wrap all routes with the Layout */}
-      <Route path="/" element={<Layout />}>
+      {/* <Route path="/" element={<Layout />}> */}
         {/* Define all the individual routes inside Layout */}
-        <Route index element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
         <Route path="role" element={<RoleSelection />} />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
         <Route path="admin" element={<AdminDashboard />} />
         <Route path="teacher" element={<TeacherDashboard />} />
         <Route path="student" element={<StudentDashboard />} />
-      </Route>
+      {/* </Route> */}
     </Routes>
   );
 };
